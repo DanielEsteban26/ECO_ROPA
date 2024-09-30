8ef016bbc06de85f2c96dbdbf2a22518c7e4ce6b
Daniel Isidro <isidroesteban402@gmail.com>
2024-09-29 22:40:26 -0500

    creando proyecto ECO_ROPA

---
+++ src/main/java/pe/edu/cibertec/eco_ropa/EcoRopaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.eco_ropa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoRopaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoRopaApplication.class, args);
+    }
+
+}
